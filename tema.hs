6d188212245264dd7228e36b2d7f12d7bb2a55f9
juankaplan <jkaplan2004@gmail.com>
2023-03-20 20:53:20 -0300

    module Tema y Tipos ready

--- tema.hs
+++ tema.hs
@@ -1,5 +1,27 @@
 module Tema (Tema, nuevoT, nombreT, datosT, etiquetasT, agregarT, aplicaT )
 where
 
-import Tipos
+import Tipos 
 data Tema = Tem Nombre [ Etiqueta ] Datos deriving (Eq, Show, Ord)
+
+nuevoT :: Nombre -> Datos -> Tema
+nuevoT nombre = Tem nombre [] 
+
+nombreT :: Tema -> Nombre
+nombreT (Tem nombre etiquetas datos) = nombre
+
+datosT :: Tema -> Datos
+datosT (Tem nombre etiquetas datos) = datos
+
+etiquetasT :: Tema -> [ Etiqueta ]
+etiquetasT (Tem nombre etiquetas datos) = etiquetas
+
+agregarT :: Etiqueta -> Tema -> Tema
+agregarT etiqueta (Tem nombre etiquetas datos) = Tem nombre new_etiquetas datos 
+   where
+    new_etiquetas = insertar etiqueta etiquetas
+
+aplicaT :: Etiqueta -> Tema -> Bool
+aplicaT etiqueta (Tem nombre etiquetas datos) = foldl (\fold each-> (||) (each == etiqueta) fold) False etiquetas
+---(each == etiqueta) || fold
+
--- tipos.hs
+++ tipos.hs
@@ -1,5 +1,13 @@
-module Tipos where
+module Tipos (Datos, Etiqueta, Nombre, insertar) where
 
 type Datos = String
 type Etiqueta = String
 type Nombre = String
+
+sortFunction :: Ord a => a -> [a] -> [a]
+sortFunction x list = sorted ++ [x] ++ drop (length sorted) list
+   where
+   sorted= [y | y <- list, y < x ]
+
+insertar :: Ord a => a -> [ a ] -> [ a ]
+insertar x list = foldr sortFunction [] (list ++ [x])
