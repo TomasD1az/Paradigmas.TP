979a5364062d24ae05a95a29edddd6935b4f065b
juankaplan <jkaplan2004@gmail.com>
2023-03-24 15:53:09 -0300

    agrego carpeta tp1

--- tp1/playlist.hs
+++ tp1/playlist.hs
@@ -10,7 +10,7 @@ nuevaP = Play 0
 
 -- Dada una Playlist devuelve el tema en la posicion indicada por el indice.
 actualP :: Playlist -> Tema
-actualP (Play Int temas) = temas !! Int -- hacer test
+actualP (Play int temas) = temas !! int -- hacer test
 
 --Devuelve una Playlist con su indice aumentado en uno.
 skipP :: Playlist -> Playlist
@@ -22,4 +22,4 @@ backP (Play i temas) = Play (i - 1) temas
 
 -- Dada una Playlist crea una nueva con la lista de temas de la original.
 resetP :: Playlist -> Playlist
-resetP (Play _ temas) = nuevaP [] --no se si se van a meter los valores en la funcion nuevaP, hacer test
+resetP (Play _ temas) = nuevaP temas --no se si se van a meter los valores en la funcion nuevaP, hacer test
