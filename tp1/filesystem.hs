ebbaae03468f57fc3c91c54139ba482965af5f41
Tomas Diaz <tdiaz@udesa.edu.ar>
2023-03-24 18:56:40 -0300

    2nda parte de filessystem

--- tp1/filesystem.hs
+++ tp1/filesystem.hs
@@ -2,8 +2,8 @@ module FileSystem (printT, nuevoF, etiquetasF)
 where
 
 import Tipos
-import Tema ( Tema, nuevoT )
-data FileSystem = FS [Etiqueta] [Tema] deriving (Eq, Show)
+import Tema
+data FileSystem = FS [Etiqueta] [Tema] deriving (Eq)
 
 instance Show FileSystem
      where show (FS etiquetas temas) = "FileSystem\n" ++ "| Etiquetas: " ++ show etiquetas ++ "\n| Temas:\n" ++ printT temas
@@ -14,4 +14,22 @@ nuevoF :: FileSystem
 nuevoF = FS [] [] -- hacer test
 
 etiquetasF :: FileSystem -> [ Etiqueta ]
-etiquetasF (FS etiquetas _ ) = etiquetas !! -- no se si esto esta bien, tampoco se que pide
+etiquetasF (FS etiquetas _ ) = etiquetas -- test
+
+temasF :: FileSystem -> [ Tema ]
+temasF (FS _ temas) = temas -- test
+
+-- Agrega el tema y sus etiquetas de ser necesario.
+-- agregarF :: Tema -> FileSystem -> FileSystem
+-- agregarF tema (FS etiquetas temas) = filesystem
+
+filtrarF :: Etiqueta -> FileSystem -> [ Tema ]
+filtrarF etiqueta (FS etiquetas temas) = foldr (\each fold-> if elem (etiqueta etiquetasT each) then each:fold else fold) []
+
+tema1 = nuevoT "cancion1" "caca1"
+temazo = agregarT "Rock" tema1
+
+tema2 = nuevoT "cancion2" "caca2"
+temazono = agregarT "" tema2
+
+filesys1 = FS [] [temazo, temazono]
